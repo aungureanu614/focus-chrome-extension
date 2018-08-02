85b3e7f5e133fdfc8c8ca8efc008f3a74290d639
Ana Maria Chaleunrath <aungureanu614@gmail.com>
2018-08-01 17:14:34 -0700

    stuff

--- main.js
+++ main.js
@@ -10,3 +10,30 @@
 // parent.appendChild(image);
 // parent.classList.add('align');
 // text.classList.add('beautText');
+
+class Focus {
+    constructor() {
+       
+    }
+
+    async getRandomImage() {
+        const url = 'https://api.unsplash.com/photos/random?client_id=871cf564f90068ebe15bd15bfd95cc0f8366474ee386c149d36bf45f89bedf19'
+    
+        let response = await fetch(url);
+        return await response.json();        
+    }
+
+    displayImage(randomImg) {
+        const content = document.getElementById('page-manager').children[0];
+        content.parentNode.removeChild(content);
+        const parent = document.getElementById('page-manager');
+        const image = document.createElement('img');
+        image.src = randomImg;
+        parent.appendChild(image);
+    }
+}
+
+let youtube = new Focus();
+let randomImg = youtube.getRandomImage();
+console.log(randomImg)
+youtube.displayImage(randomImg);
