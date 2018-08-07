2ea18f8eaeaa711cf858f3eaf55fe25780d7c881
Ana Maria Chaleunrath <aungureanu614@gmail.com>
2018-08-07 13:48:12 -0700

    restructure code more

--- focus_init.js
+++ focus_init.js
@@ -2,14 +2,16 @@ import Focus from './main';
 
 const initExtension = async () => {
 let youtube = new Focus('page-manager');
-await youtube.getRandomImage()
+youtube.addStyleClass('content__align', youtube.content)
 
-youtube.addText();
- 
+const h2 = youtube.addText();
+youtube.addStyleClass('beautText', h2)
+// await youtube.getRandomImage('inspirational');
 // TODO uncomment below after done
-//  let randomImg = await youtube.getRandomImage();
-//  youtube.displayImage(randomImg, content);
-youtube.displayImage(chrome.extension.getURL('images/pic.jpeg'), content);
+ let randomImg = await youtube.getRandomImage('inspirational');
+ const image = youtube.setAndDisplayImage(randomImg);
+// const image = youtube.setAndDisplayImage(chrome.extension.getURL('images/pic.jpeg'));
+youtube.addStyleClass('image-style', image);
 };
 
 initExtension();
--- main.js
+++ main.js
@@ -4,11 +4,10 @@ import Unsplash from 'unsplash-js';
 export default class Focus {
     constructor(selector) {
         this.content = document.getElementById(selector);
-        this.content.classList.add('content__align');
-        this.removeContent();
+        this._removeContent();
     }
 
-    async getRandomImage() {
+    async getRandomImage(queryType) {
         const unsplash = new Unsplash({
             applicationId: auth.applicationId,
             secret: auth.secret,
@@ -18,27 +17,31 @@ export default class Focus {
             }
         })
 
-        let response = await unsplash.photos.getRandomPhoto({query: "inspirational"})
+        let response = await unsplash.photos.getRandomPhoto({query: queryType})
         let responseJson = await response.json(); 
         return responseJson.urls.regular; 
     }
 
-    removeContent() {
+    _removeContent() {
         const childContent = this.content.children[0];
         childContent.parentNode.removeChild(childContent);
     }
 
-    displayImage(randomImg) {
+    addStyleClass(className, el) {
+        el.classList.add(className);
+    }
+
+    setAndDisplayImage(randomImg) {
         const image = new Image();
         image.src = randomImg;
-        image.classList.add('image-style');
         this.content.appendChild(image);
+        return image;
     }
 
     addText() {
         const h2 = document.createElement('h2');
         h2.innerText = "Get back to building your dreams!";
-        h2.classList.add('beautText');
         this.content.appendChild(h2);
+        return h2;
     }
 }
