48cffe7727f5d3f14aebd5e0248ad12329a41491
Ana Maria Chaleunrath <aungureanu614@gmail.com>
2018-08-08 15:19:24 -0700

    eslint

--- focus_init.js
+++ focus_init.js
@@ -1,31 +1,30 @@
 import Focus from './main';
 
 const initExtension = async () => {
+  const youtube = new Focus('page-manager');
+  youtube.addStyleClass('content__align', youtube.content);
 
-const youtube = new Focus('page-manager');
-youtube.addStyleClass('content__align', youtube.content)
+  // get quote of the day
+  const quoteObj = await youtube.getQuote('inspire');
+  const quoteInfo = quoteObj.contents.quotes[0];
+  const h2 = youtube.createText('h2');
+  h2.innerHTML = `${quoteInfo.quote} <div class="heading--author__display">--${quoteInfo.author}</div>`;
+  youtube.addStyleClass('beautText', h2);
 
-// get quote of the day
-const quoteObj = await youtube.getQuote('inspire');
-const quoteInfo = quoteObj.contents.quotes[0];
-const h2 = youtube.createText('h2');
-h2.innerHTML = `${quoteInfo.quote} <div class="heading--author__display">--${quoteInfo.author}</div>`;
-youtube.addStyleClass('beautText', h2)
+  // get inspirational image
+  const randomImg = await youtube.getRandomImage('inspirational');
+  const imageObj = new Image();
+  const image = youtube.setAndDisplayImage(randomImg, imageObj);
+  youtube.addStyleClass('image-style', image);
 
-// get inspirational image
-const randomImg = await youtube.getRandomImage('inspirational');
-const imageObj = new Image();
-const image = youtube.setAndDisplayImage(randomImg, imageObj);
-youtube.addStyleClass('image-style', image);
-
-image.addEventListener('click', async(e) => {
-    let newRandom = await youtube.getRandomImage('inspirational');
+  image.addEventListener('click', async () => {
+    const newRandom = await youtube.getRandomImage('inspirational');
     youtube.setAndDisplayImage(newRandom, image);
-})
+  });
 
-// clear top right controls
-const topButtons = document.getElementById('buttons');
-youtube.removeContent(topButtons);
+  // clear top right controls
+  const topButtons = document.getElementById('buttons');
+  youtube.removeContent(topButtons);
 };
 
 initExtension();
--- main.js
+++ main.js
@@ -1,50 +1,50 @@
-import auth from './auth';
 import Unsplash from 'unsplash-js';
+import auth from './auth';
 
 export default class Focus {
-    constructor(selector) {
-        this.content = document.getElementById(selector);
-        this.removeContent();
-    }
+  constructor(selector) {
+    this.content = document.getElementById(selector);
+    this.removeContent();
+  }
 
-    async getRandomImage(imgTag) {
-        const unsplash = new Unsplash({
-            applicationId: auth.applicationId,
-            secret: auth.secret,
-            callbackUrl: auth.callbackUrl,
-            headers: {
-                mode: 'no-cors'
-            }
-        })
+  async getRandomImage(imgTag) {
+    const unsplash = new Unsplash({
+      applicationId: auth.applicationId,
+      secret: auth.secret,
+      callbackUrl: auth.callbackUrl,
+      headers: {
+        mode: 'no-cors',
+      },
+    });
 
-        const response = await unsplash.photos.getRandomPhoto({query: imgTag})
-        const responseJson = await response.json(); 
-        return responseJson.urls.regular; 
-    }
+    const response = await unsplash.photos.getRandomPhoto({ query: imgTag });
+    const responseJson = response.json();
+    return responseJson.urls.regular;
+  }
 
-    async getQuote(category) {
-        const url = `https://quotes.rest/qod?category=${category}`;
-        const response = await fetch(url);
-        return await response.json();
-    }
+  async getQuote(category) {
+    const url = `https://quotes.rest/qod?category=${category}`;
+    const response = await fetch(url);
+    return response.json();
+  }
 
-    removeContent(el = this.content) {
-        el.innerHTML = '';
-    }
+  removeContent(el = this.content) {
+    el.innerHTML = '';
+  }
 
-    addStyleClass(className, el) {
-        el.classList.add(className);
-    }
+  addStyleClass(className, el) {
+    el.classList.add(className);
+  }
 
-    setAndDisplayImage(randomImg, image) {
-        image.src = randomImg;
-        this.content.appendChild(image);
-        return image;
-    }
+  setAndDisplayImage(randomImg, image) {
+    image.src = randomImg;
+    this.content.appendChild(image);
+    return image;
+  }
 
-    createText(el) {
-        const elem = document.createElement(el);
-        this.content.appendChild(elem);
-        return elem;
-    }
+  createText(el) {
+    const elem = document.createElement(el);
+    this.content.appendChild(elem);
+    return elem;
+  }
 }
