2d89ddd9adf675528b8e82022ac49a681bd450a1
Ana Maria Chaleunrath <aungureanu614@gmail.com>
2018-08-07 18:46:06 -0700

    add comments

--- focus_init.js
+++ focus_init.js
@@ -1,19 +1,23 @@
 import Focus from './main';
 
 const initExtension = async () => {
+
 let youtube = new Focus('page-manager');
 youtube.addStyleClass('content__align', youtube.content)
 
-const h2 = youtube.createText('h2');
+// get quote of the day
 let quoteObj = await youtube.getQuote('inspire');
 let quoteInfo = quoteObj.contents.quotes[0];
+const h2 = youtube.createText('h2');
 h2.innerHTML = `${quoteInfo.quote} <div class="heading--author__display">--${quoteInfo.author}</div>`;
 youtube.addStyleClass('beautText', h2)
 
+// get inspirational image
 let randomImg = await youtube.getRandomImage('inspirational');
 const image = youtube.setAndDisplayImage(randomImg);
-
 youtube.addStyleClass('image-style', image);
+
+// clear top right controls
 const topButtons = document.getElementById('buttons');
 youtube.removeContent(topButtons);
 };
